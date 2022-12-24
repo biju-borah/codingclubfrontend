260dae753af1797ae6bc61ba5974813c5a3224d7
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-24 19:48:44 +0530

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,32 +3,32 @@ import './style.css'
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg bg-light nav-tran" id="navbar">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-tran" id="navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
                     <img src="/src/assets/react.svg" alt="Bootstrap" width="30" style={{ marginLeft: '1rem' }} />
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Article</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Article</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Hall of Fame</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Hall of Fame</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Alumni</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Alumni</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Our Team</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Our Team</a>
                         </li>
                     </ul>
                 </div>
