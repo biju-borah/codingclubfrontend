dfadc3b092fdf0ee9cd78ddf2495c52126c5cfc5
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-25 13:27:51 +0530

    added logos

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -10,20 +10,20 @@ export default function Hero() {
                     <div className=" Typewriter" data-testid="typewriter-wrapper" style={{ fontFamily: 'monospace' }}>Coding is <span
                         className="Typewriter__wrapper"></span><span className="Typewriter__cursor cursor"> </span></div>
                 </div>
+                <div className='socialHandle'>
+                    <div className='socialIcon'>
+                        <a href="#" target="_blank"><img src="/logos/linkedin.svg" alt="linkedin" /></a>
+                    </div>
+                    <div className='socialIcon'>
+                        <a href="#" target="_blank"><img src="/logos/facebook.svg" alt="facebook" /></a>
+                    </div>
+                    <div className='socialIcon'>
+                        <a href="#" target="_blank"><img src="/logos/instagram.svg" alt="instagram" /></a>
+                    </div>
+                    <div className='verticalLine'></div>
+                </div>
             </div>
             <canvas id="banner_canvas" className="transparent_effect" width="1233" height="601"></canvas>
-            <div className='socialHandle'>
-                <div className='socialIcon'>
-                    <a href="#" target="_blank"><img src="/logos/linkedin.svg" alt="linkedin" /></a>
-                </div>
-                <div className='socialIcon'>
-                    <a href="#" target="_blank"><img src="/logos/facebook.svg" alt="facebook" /></a>
-                </div>
-                <div className='socialIcon'>
-                    <a href="#" target="_blank"><img src="/logos/instagram.svg" alt="instagram" /></a>
-                </div>
-                <div className='verticalLine'></div>
-            </div>
         </section>
     )
 }
