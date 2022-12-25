35a630b4be3b6483f0dd9e21e381557ace8488a9
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-25 14:52:41 +0530

    about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,7 +3,20 @@ import './style.css'
 
 function About() {
   return (
-    <></>
+    <div className='about_shape_bg'>
+      <div className="row align-items-center">
+        <div className="col-lg-8 offset-lg-2 col-md-12 col-sm-12">
+          <div className="title_default_dark title_border text-center">
+            <h4 className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style={{ animationDelay: "0.2s", opacity: "1" }}>About The Cryptocash</h4>
+            <p className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style={{ animationDelay: "0.4s", opacity: "1" }}>Cryptocash is one of the most transformative
+              technologies since the invention of the Internet. Cryptocash stands firmly in support of
+              financial freedom and the liberty that Bitcoin provides globally for anyone to voluntarily
+              participate in a permissionless and decentralized network.</p>
+            <a href="https://bestwebcreator.com/cryptocash/demo/about.html" className="btn btn-info box_shadow animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.6s" style={{ animationDelay: "0.6s", opacity: "1" }}>Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
