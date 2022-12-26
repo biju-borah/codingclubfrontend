b125287bf807e3ee12e6174e504d9f5c9e56f556
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-26 18:02:01 +0530

    checkpoint

--- src/components/Archive/Contest/Contest.jsx
+++ src/components/Archive/Contest/Contest.jsx
@@ -6,67 +6,40 @@ function Contest() {
         <div className='contest_bg section_gradiant3'>
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-6 offset-lg-3 col-md-12 col-sm-12">
-                        <div className="title_default_light text-center">
-                            <h4 className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style={{ animationDelay: "0.2s", opacity: "1" }}>Token Structure</h4>
-                            <p className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style={{ animationDelay: "0.4s", opacity: "1" }}>Join the industry leaders to discuss where the
-                                markets are heading. We accept token payments. </p>
+                    <div className="col-lg-8 offset-lg-2 col-md-12 col-sm-12">
+                        <div className="title_default_light title_border text-center">
+                            <h4 className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style={{ animationDelay: "0.2s", opacity: "1" }}>Coding Archive</h4>
+                            <p className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style={{ animationDelay: "0.4s", opacity: "1" }}>Almost everything today runs on code. Coding is one of the most essential skills for an engineer. With the same outlook in mind, the coding club strives to teach the students to code. It trains students in the commonest languages from beginner to advanced levels. NIT Silchar proudly flaunts some of the best coders in the nation.
+                                participate in a permissionless and decentralized network.</p>
                         </div>
                     </div>
                 </div>
                 <div className="row align-items-center">
                     <div className="col-lg-3">
                         <div className="card text-center animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style={{ animationDelay: "0.2s", opacity: "1" }}>
-                            <h5>Private Pre-Sale</h5>
-                            <span>June 20, 2018</span>
+                            <img src="/vite.svg" className="card-img-top img-fluid p-5" alt="card" />
                         </div>
                         <div className="card text-center animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style={{ animationDelay: "0.4s", opacity: "1" }}>
-                            <h5>Pre-Sale</h5>
-                            <span>July 18, 2018</span>
+                            <img src="/vite.svg" className="card-img-top img-fluid p-5" alt="card" />
                         </div>
                         <div className="card text-center animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.6s" style={{ animationDelay: "0.6s", opacity: "1" }}>
-                            <h5>Crowdsale</h5>
-                            <span>February 20, 2018</span>
+                            <img src="/vite.svg" className="card-img-top img-fluid p-5" alt="card" />
                         </div>
                     </div>
                     <div className="col-lg-6">
-                        <div className="token_sale res_md_mt_20">
-                            <div className="tk_countdown bg-white-tran text-center animation animated fadeIn" data-animation="fadeIn" data-animation-delay="0.6s" style={{ animationDelay: "0.6s", opacity: "1" }}>
-                                <div className="tk_counter_inner">
-                                    <div className="tk_countdown_time box_shadow_none transparent_bg animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.7s" data-time="2021/12/08 00:00:00" style={{ animationDelay: "0.7s", opacity: "1" }}><span className="counter_box"><span className="tk_counter days">00 </span><span className="tk_text">Days</span></span><span className="counter_box"><span className="tk_counter hours">00</span><span className="tk_text">Hours</span></span><span className="counter_box"><span className="tk_counter minutes">00</span><span className="tk_text">Minutes</span></span><span className="counter_box"><span className="tk_counter seconds">00</span><span className="tk_text">Seconds</span></span></div>
-                                    <div className="progress animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.8s" style={{ animationDelay: "0.8s", opacity: "1" }}>
-                                        <div className="progress-bar progress-bar-striped gradient_box" role="progressbar" aria-valuenow="46" aria-valuemin="0" aria-valuemax="100" style={{ left: "46%" }}> 46%
-                                        </div>
-                                        <span className="progress_label bg-white" style={{ left: "30%" }}> <strong> 46,000 BCC
-                                        </strong></span>
-                                        <span className="progress_label bg-white" style={{ left: "75%" }}> <strong> 90,000 BCC
-                                        </strong></span>
-                                        <span className="progress_min_val">Sale Raised</span>
-                                        <span className="progress_max_val">Soft-caps</span>
-                                    </div>
-                                    <a href="https://bestwebcreator.com/cryptocash/demo/index-cyan-blue-modern.html#" className="btn btn-default animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.9s" style={{ animationDelay: "0.9s", opacity: "1" }}>Buy Tokens <i className="ion-ios-arrow-thin-right"></i></a>
-                                    <ul className="icon_list list_none d-flex justify-content-center">
-                                        <li className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="1s" style={{ animationDelay: "1s", opacity: "1" }}><i className="fa fa-cc-visa"></i></li>
-                                        <li className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="1.1s" style={{ animationDelay: "1.1s", opacity: "1" }}><i className="fa fa-cc-mastercard"></i></li>
-                                        <li className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="1.2s" style={{ animationDelay: "1.2s", opacity: "1" }}><i className="fa fa-bitcoin"></i></li>
-                                        <li className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="1.3s" style={{ animationDelay: "1.3s", opacity: "1" }}><i className="fa fa-paypal"></i></li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div className="big_card text-center animation animated fadeIn" data-animation="fadeIn" data-animation-delay="0.6s" style={{ animationDelay: "0.6s", opacity: "1" }}>
+                            <img src="/vite.svg" className="card-img-top img-fluid p-5" alt="card" />
                         </div>
                     </div>
                     <div className="col-lg-3">
                         <div className="card text-center animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style={{ animationDelay: "0.2s", opacity: "1" }}>
-                            <h5>Low - High 24h</h5>
-                            <span>$ 6,455.83 - $ 7,071.42</span>
+                            <img src="/vite.svg" className="card-img-top img-fluid p-5" alt="card" />
                         </div>
                         <div className="card text-center animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style={{ animationDelay: "0.4s", opacity: "1" }}>
-                            <h5>Total Token Sale</h5>
-                            <span>855,000 BCC (8.2%)</span>
+                            <img src="/vite.svg" className="card-img-top img-fluid p-5" alt="card" />
                         </div>
                         <div className="card text-center animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.6s" style={{ animationDelay: "0.6s", opacity: "1" }}>
-                            <h5>Acceptable Currency </h5>
-                            <span>BTC, ETH, LTC</span>
+                            <img src="/vite.svg" className="card-img-top img-fluid p-5" alt="card" />
                         </div>
                     </div>
                 </div>
