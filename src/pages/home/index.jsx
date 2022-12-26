dce29d33854b4cf16e0bfb9e2bdba01bf05f1006
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-26 16:49:00 +0530

    checkpoint

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from '../../components/About'
+import Contest from '../../components/Archive/Contest/Contest'
 import Interview from '../../components/Archive/Interview/Interview'
 import Hero from '../../components/Hero'
 import Navbar from '../../components/Navbar'
@@ -13,6 +14,7 @@ function Home() {
             <About />
             <Interview />
             <Youtube />
+            <Contest />
         </>
     )
 }
