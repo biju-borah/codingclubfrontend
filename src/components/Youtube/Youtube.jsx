1b9d2e5eb35d1fb56a956604c4a08c74cf4d99ef
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-26 15:43:32 +0530

    updated deployment error

--- src/components/Youtube/Youtube.jsx
+++ src/components/Youtube/Youtube.jsx
@@ -11,7 +11,7 @@ function Youtube() {
                         <h4 className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style={{ animationDelay: "0.2s", opacity: "1" }}>Our Youtube</h4>
                         <p className="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style={{ animationDelay: "0.4s", opacity: "1" }}>Almost everything today runs on code. Coding is one of the most essential skills for an engineer. With the same outlook in mind, the coding club strives to teach the students to code. It trains students in the commonest languages from beginner to advanced levels. NIT Silchar proudly flaunts some of the best coders in the nation.
                             participate in a permissionless and decentralized network.</p>
-                        <YoutubeCard />
+                        {/* <YoutubeCard /> */}
                         <button className="button animation animated fadeInUp mt-3" data-animation="fadeInUp" data-animation-delay="0.6s" style={{ animationDelay: "0.6s", opacity: "1" }}>Explore More</button>
 
                     </div>
