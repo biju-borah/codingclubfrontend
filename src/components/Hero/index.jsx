54943c3bd8d9ea1dee09e3cbd7b699da538aa5ac
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-28 21:12:58 +0530

    fixed typing

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { TypeAnimation } from 'react-type-animation'
 import './style.css'
 
 export default function Hero() {
@@ -8,9 +9,24 @@ export default function Hero() {
             <div id="hero" className="landing-main">
                 <h1 data-aos="fade-up" className="name aos-init aos-animate" style={{ zIndex: '10' }}>Coding Club</h1>
                 <div data-aos="fade-up" className="name-2 aos-init aos-animate" style={{ zIndex: '10' }}>
-                    <div className=" Typewriter" data-testid="typewriter-wrapper" style={{ fontFamily: 'monospace' }}>Coding is <span
-                        className="Typewriter__wrapper"></span><span className="Typewriter__cursor cursor"> </span></div>
+                    {/* <div className=" Typewriter" data-testid="typewriter-wrapper" style={{ fontFamily: 'monospace' }}>Coding is <span
+                        className="Typewriter__wrapper"></span><span className="Typewriter__cursor cursor"> </span></div> */}
+                    <TypeAnimation sequence={[
+                        'Coding is Love',
+                        2500,
+                        'Coding is Fun',
+                        2500,
+                        'Coding is a journey',
+                        2500,
+                        'Coding is Life',
+                        2500,
+                    ]}
+                        wrapper="div"
+                        cursor={true}
+                        repeat={Infinity}
+                        style={{ fontFamily: 'monospace' }} />
                 </div>
+
                 <div className='socialHandle'>
                     <div className='socialIcon'>
                         <a href="#" target="_blank"><img src="/logos/linkedin.svg" alt="linkedin" /></a>
